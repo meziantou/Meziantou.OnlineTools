78efafc182fd234b4d0e6497aeb48c7792d9ca54
Gérald Barré <git@meziantou.net>
2026-06-12 19:02:30 -0400

    Add hexa tool link and download script

---
+++ Meziantou.OnlineTools/wwwroot/js/download.js
@@ -0,0 +1,11 @@
+window.downloadFileFromStream = async (fileName, contentStreamReference) => {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = fileName;
+    anchor.click();
+    anchor.remove();
+    URL.revokeObjectURL(url);
+};
